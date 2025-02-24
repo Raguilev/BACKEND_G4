11d3e9f088a2f6b8c1f20154bb4b4223140e9825
JML037 <20192962@aloe.ulima.edu.pe>
2025-02-24 01:35:06 -0500

    Obtener todos los usuarios en el Back

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -7,6 +7,30 @@ const UserController = () => {
 
   //Agregar otros endpoints, post, get
 
+  // Ruta para obtener todos los usuarios
+  router.get("/list", async (req: Request, resp: Response) => {
+    try {
+      const users = await db.User.findAll(); // Recupera todos los usuarios de la base de datos
+      resp.json({
+        msg: "Lista de usuarios",
+        users: users,
+      });
+    } catch (error: unknown) {  // Cambié el tipo a `unknown`
+      if (error instanceof Error) {
+        resp.status(500).json({
+          msg: "Error al obtener los usuarios",
+          error: error.message,
+        });
+      } else {
+        resp.status(500).json({
+          msg: "Error desconocido al obtener los usuarios",
+          error: "Error desconocido",
+        });
+      }
+    }
+  });
+  
+
   router.get("/totalUsers", async (req: Request, resp: Response) => {
     const totalUsers = await db.User.count({
       where:{
