c8dfb0cb71dda2303c3899b3fc0e21cb5dfba58b
Paztelazo <20211960@aloe.ulima.edu.pe>
2025-02-26 22:38:23 -0500

    faltaba detallito al seeder de budget

--- src/DAO/seeders/20250224211143-data_budgets.js
+++ src/DAO/seeders/20250224211143-data_budgets.js
@@ -4,27 +4,12 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     
     await queryInterface.bulkInsert("Budget", [
-     {
-      user_id : 2,
-      category_id: 1,
-      monthly_budget: 129.99
-     },
-     {
-      user_id : 3,
-      category_id: 3,
-      monthly_budget: 222.22
-     },
-     {
-      user_id : 2,
-      category_id: 2,
-      monthly_budget: 1229.99
-     },
-     {
-      user_id : 2,
-      category_id: 3,
-      monthly_budget: 779.99
-     }
-     
+      { user_id: 2, category_id: 1, monthly_budget: 129.99 },
+      { user_id: 3, category_id: 3, monthly_budget: 222.22 },
+      { user_id: 2, category_id: 2, monthly_budget: 1229.99 },
+      { user_id: 2, category_id: 3, monthly_budget: 779.99 },
+      { user_id: 4, category_id: 1, monthly_budget: 350.00 }, // Agregar para usuario 4
+      { user_id: 5, category_id: 2, monthly_budget: 540.00 }  // Agregar para usuario 5
     ], {});
   },
   async down (queryInterface, Sequelize) {
